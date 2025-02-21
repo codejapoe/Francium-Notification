c3cb9ac6aa24370b8ce56b4268d9357f1aa0d29a
Codejapoe LFT <98520092+codejapoe@users.noreply.github.com>
2025-02-21 03:09:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -442,6 +442,6 @@ app.post("/post", async function (req, res) {
     }
 });
   
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+app.listen(80, function () {
+    console.log("Server started on port 80");
 });
